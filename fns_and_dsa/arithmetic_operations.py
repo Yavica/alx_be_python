acde5ddf796f773511e98eb854e7d235ae81a223
Yavica <jamiemaina06@gmail.com>
2025-07-27 13:45:37 +0300

    fix: Adjust perform_operation signature to pass checks

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,6 @@
 # fns_and_dsa/arithmetic_operations.py
 
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     """
     Performs basic arithmetic operations between two numbers.
 
